04bbe3e7df6b4cf6733e096f5a62d7871f6ebc3e
ImJustLucas <bellierlucas.pro@gmail.com>
2023-01-07 22:12:02 +0100

    🏗️ architectural changes for css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 
 import { Layout } from "@layouts/layout";
 
-import "@styles/reset.css";
+import "@styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
