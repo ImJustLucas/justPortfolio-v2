95681a125d8790c456d7cf1ad1759850f982e6d8
ImJustLucas <bellierlucas.pro@gmail.com>
2023-01-07 23:56:16 +0100

    🚧 work on menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,35 @@
 import styled from "styled-components";
 
+import { NavBar } from "@components/NavBar";
+
 export const Menu = () => {
-  return <Container>Menu</Container>;
+  return (
+    <Container>
+      <TitleContainer>
+        <Title>Lucas.</Title>
+      </TitleContainer>
+      <NavBar />
+    </Container>
+  );
 };
 
 const Container = styled.div`
   width: 30%;
   height: 100vh;
-  background-color: #f5f5f5;
+  background-color: var(--color-lightblue);
+  padding: 40px 0;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const Title = styled.h1`
+  font-family: "Cormorant Unicase", serif;
+  font-size: 5rem;
+  text-transform: uppercase;
+  color: var(--color-dark);
 `;
