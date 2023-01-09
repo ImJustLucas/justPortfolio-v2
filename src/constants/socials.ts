4bd53fa34bf859a9849edf8696f2f609abc542f5
ImJustLucas <bellierlucas.pro@gmail.com>
2023-01-09 16:44:19 +0100

    🚚 rename constants name

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,8 +1,8 @@
 const SOCIALS = {
-  github: "https://github.com/ImJustLucas",
-  linkedin: "https://www.linkedin.com/in/lucasbellier/",
-  twitter: "https://twitter.com/ImJustLucas_",
-  instagram: "https://www.instagram.com/luuucas.blr/",
+  GITHUB: "https://github.com/ImJustLucas",
+  LINKEDIN: "https://www.linkedin.com/in/lucasbellier/",
+  TWITTER: "https://twitter.com/ImJustLucas_",
+  INSTAGRAM: "https://www.instagram.com/luuucas.blr/",
 };
 
 export default SOCIALS;
