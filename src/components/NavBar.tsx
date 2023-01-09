7d26be4a88da73f161fad9f1123e6227659f4c92
ImJustLucas <bellierlucas.pro@gmail.com>
2023-01-09 16:15:31 +0100

    💫 add animation on active border

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,31 @@
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 import { NavItem } from "@components/NavItem";
 
 export const NavBar = () => {
+  const router = useRouter();
+  const page = router.pathname;
+  let activePath: string = "0";
+
+  switch (page) {
+    case "/" || null:
+      activePath = "0";
+      break;
+    case "/about":
+      activePath = "60";
+      break;
+    case "/tech":
+      activePath = "120";
+      break;
+    case "/contact":
+      activePath = "180";
+      break;
+  }
+  console.log(activePath);
+
   return (
-    <Navbar>
+    <Navbar activePath={activePath}>
       <NavItem link="/" name="me" />
       <NavItem link="/about" name="all my works" />
       <NavItem link="/tech" name="tech i use" />
@@ -13,10 +34,21 @@ export const NavBar = () => {
   );
 };
 
-const Navbar = styled.nav`
+const Navbar = styled.nav<{ activePath?: string }>`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 80px 0;
+  margin-top: 80px;
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 60px;
+    border-right: 3px solid var(--color-darkblue);
+    top: ${({ activePath }) => activePath}px;
+    transition: all 0.2s ease-in-out;
+  }
 `;
