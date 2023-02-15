9c8e3695c2be5b971e503532a68f417fc4e090a1
ImJustLucas <bellierlucas.pro@gmail.com>
2023-02-15 20:14:05 +0100

    🚧 add work card component

---
+++ src/components/WorkCard.tsx
@@ -0,0 +1,7 @@
+// import styled from 'styled-components'
+import { WorkType } from '@typesDef/work'
+
+export const WorkCard = ({ work }: { work: WorkType }) => {
+  console.log(work)
+  return <p>work card</p>
+}
