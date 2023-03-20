5f09b1d4b98e883681777846a9983883202bb9f7
ImJustLucas <bellierlucas.pro@gmail.com>
2023-03-20 22:19:10 +0100

    🧐 add tech data

---
+++ src/data/technologies.ts
@@ -0,0 +1,42 @@
+import { TechType } from '@typesDef/tech'
+
+const Technologies: TechType[] = [
+  {
+    name: 'Nuxt',
+    description:
+      'Nuxt.js is a free and open source web application framework based on Vue.js & Node.js',
+    image: '/images/technologies/nuxt.svg',
+  },
+  {
+    name: 'Next',
+    description:
+      'Next.js is a free and open source web application framework based on React.js developed by Vercel',
+    image: '/images/technologies/next.svg',
+  },
+  {
+    name: 'TypeScript',
+    description:
+      'TypeScript is a free and open source programming language based on Javascript developed and maintained by Microsoft',
+    image: '/images/technologies/typescript.svg',
+  },
+  {
+    name: 'Express',
+    description:
+      'Express.js is a free and open source web application framework for Node.js',
+    image: '/images/technologies/express.svg',
+  },
+  {
+    name: 'Vue',
+    description:
+      'Vue.js is a free and open source web application framework based on Javascript',
+    image: '/images/technologies/vue.svg',
+  },
+  {
+    name: 'React',
+    description:
+      'React is a free and open source web application framework based on Javascript',
+    image: '/images/technologies/react.svg',
+  },
+]
+
+export default Technologies
