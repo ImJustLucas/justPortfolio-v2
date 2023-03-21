69744ae014aea106f121e32e000250ae8afea7b5
ImJustLucas <bellierlucas.pro@gmail.com>
2023-03-21 20:17:16 +0100

    ✨ add tech page

--- src/components/Tech/TechRow.tsx
+++ src/components/Tech/TechRow.tsx
@@ -1,27 +1,26 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 
-export const TechRow = () => {
+import { TechType } from '@typesDef/tech'
+
+export const TechRow = ({ technologie }: { technologie: TechType }) => {
   return (
     <TechRowContainer>
       <TechHeader>
         <Image
-          src="https://via.placeholder.com/100"
+          src={technologie.image}
           width="100"
           height="100"
-          alt="nextjs"
+          alt={technologie.name}
         />
-        <TechName>Next.js</TechName>
+        <TechName>{technologie.name}</TechName>
       </TechHeader>
-      <TechDate>
-        <p>since 2023</p>
+      {/* <TechDate>
+        <p>{technologie.date} </p>
       </TechDate>
       <TechDesc>
-        <p>desc: </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, ea?
-        </p>
-      </TechDesc>
+        <p>{technologie.description}</p>
+      </TechDesc> */}
     </TechRowContainer>
   )
 }
@@ -33,6 +32,7 @@ const TechRowContainer = styled.div`
   justify-content: flex-start;
   border: 1px solid var(--color-dark);
   min-height: 100px;
+  width: 56%;
 
   &:not(:last-child) {
     margin-bottom: 1rem;
@@ -40,16 +40,19 @@ const TechRowContainer = styled.div`
 
   @media (max-width: 768px) {
     flex-direction: column;
+    width: 100%;
   }
 `
 
 const TechHeader = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-start;
+  width: 100%;
 
   @media (max-width: 768px) {
     width: 100%;
+    justify-content: space-between;
   }
 `
 
@@ -57,37 +60,46 @@ const TechName = styled.p`
   font-family: 'Cormorant Unicase';
   font-style: normal;
   font-weight: 400;
-  font-size: 42px;
+  font-size: 28px;
   padding: 0 2rem;
-`
-const TechDate = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-left: 1px solid var(--color-dark);
-  border-right: 1px solid var(--color-dark);
-  height: 100px;
-  padding: 0 2rem;
-  white-space: nowrap;
+  text-align: center;
+  width: 200px;
 
   @media (max-width: 768px) {
-    border: none;
-    border-top: 1px solid var(--color-dark);
-    border-bottom: 1px solid var(--color-dark);
+    text-align: right;
     width: 100%;
-    padding: 0;
-    height: 50px;
+    padding: 0 1rem;
   }
 `
+// const TechDate = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   border-left: 1px solid var(--color-dark);
+//   border-right: 1px solid var(--color-dark);
+//   height: 100px;
+//   padding: 0 1.4rem;
+//   white-space: nowrap;
+//   min-width: 100px;
 
-const TechDesc = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  padding: 0 2rem;
+//   @media (max-width: 768px) {
+//     border: none;
+//     border-top: 1px solid var(--color-dark);
+//     border-bottom: 1px solid var(--color-dark);
+//     width: 100%;
+//     padding: 0;
+//     height: 50px;
+//   }
+// `
 
-  @media (max-width: 768px) {
-    padding: 1.4rem;
-  }
-`
+// const TechDesc = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   justify-content: center;
+//   padding: 0 2rem;
+
+//   @media (max-width: 768px) {
+//     padding: 1.4rem;
+//   }
+// `
--- src/screens/Tech/index.tsx
+++ src/screens/Tech/index.tsx
@@ -1,3 +1,37 @@
+import styled from 'styled-components'
+
+import { TechRow } from '@components/Tech/TechRow'
+import Technologies from '@data/technologies'
+
 export default function Tech() {
-  return <>{'Tech Screen'}</>
+  return (
+    <TechContainer>
+      <TitleSection>How i create</TitleSection>
+      {Technologies.map((technologie, index) => (
+        <TechRow key={index} technologie={technologie} />
+      ))}
+    </TechContainer>
+  )
 }
+
+const TechContainer = styled.div`
+  padding: 70px 100px 0 70px;
+
+  @media (max-width: 768px) {
+    padding: 70px 50px 0 50px;
+  }
+`
+const TitleSection = styled.h2`
+  font-size: 42px;
+  font-family: 'Cormorant Unicase', serif;
+  font-weight: 400;
+  font-style: normal;
+  line-height: 50px;
+  margin-bottom: 80px;
+  text-align: left;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+    text-align: center;
+  }
+`
