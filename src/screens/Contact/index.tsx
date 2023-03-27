bfc9a7f7892d44b8b08bbfd1e0e180c41414f2d6
ImJustLucas <bellierlucas.pro@gmail.com>
2023-03-27 09:40:41 +0200

    💬 add contact title

--- src/screens/Contact/index.tsx
+++ src/screens/Contact/index.tsx
@@ -1,3 +1,31 @@
+import styled from 'styled-components'
+
 export const Contact = () => {
-  return <>{'Contact Screen'}</>
+  return (
+    <ContactContainer>
+      <TitleSection>You can reach me there</TitleSection>
+    </ContactContainer>
+  )
 }
+
+const ContactContainer = styled.div`
+  padding: 70px 100px 0 70px;
+
+  @media (max-width: 768px) {
+    padding: 70px 50px 0 50px;
+  }
+`
+
+const TitleSection = styled.h2`
+  font-size: 42px;
+  font-family: 'Cormorant Unicase', serif;
+  font-weight: 400;
+  font-style: normal;
+  line-height: 50px;
+  margin-bottom: 80px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+    text-align: center;
+  }
+`
